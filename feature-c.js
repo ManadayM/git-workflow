8daa45bdd66691394fb91a05fea47906794d45e7
Manaday Mavani <manaday.mavani@INVINDMAC3.local>
2018-06-09 17:22:03 +0530

    Feature C

---
+++ feature-c.js
@@ -0,0 +1,12 @@
+/**
+ * this feature is not so awesome,
+ * But my manager demanded it.
+ */
+
+ // he again asked me to provide more ugly stuff and patches
+ // just to please our client.. huh.
+
+
+ // one more...
+
+ // another one...
